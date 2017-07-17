ae4f225eb8969ebb959bffeb2bb0f42d9abbba1e
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-07-17 15:21:28 -0600

    toolbar appends to app.js

---
+++ src/MessageList.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+//
+// const MessageList = ({messages}) => {
+//   // console.log(messages);
+//   return(
+//     <div>
+//       {this.messages.map((element,i) => {
+//         return(
+//           <MessageList message={this.messages[i]}/>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+//
+// export default MessageList;
