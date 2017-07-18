f91ce7700cb6dea5296558a76f2a48ba7cd4f34f
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-07-18 10:35:48 -0600

    messages display in list form

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 const MessageList = ({messages}) => {
   return(
-    <div>
-    {messages.map(e =>
-      e.subject)}</div>
-    )
-  }
+    <div>{messages.map(e =>
+      <div className="row message read">
+      <div className="col-xs-1">
+      <div className="row">
+      <div className="col-xs-2">
+      <input type="checkbox" />
+      </div>
+      <div className="col-xs-2">
+      <i className="star fa fa-star-o"></i>
+      </div>
+      </div>
+      </div>
+      <div className="col-xs-11">
+      <a href='#'>
+        {e.subject}
+      </a>
+      </div>
+      </div>
+    )}
+    </div>
+)}
 
 export default MessageList;
